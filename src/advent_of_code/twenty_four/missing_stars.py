b6b8f7d791741ed3c6b84bd92adc0887e6f2e303
makejames <james@makejames.com>
2024-12-01 22:33:11 +0000

    FUNC: refactor for part two of solution

--- src/advent_of_code/twenty_four/missing_stars.py
+++ src/advent_of_code/twenty_four/missing_stars.py
@@ -1,24 +1,20 @@
 """Missing Stars Solution."""
 
 
-def missing_stars_solution(input_text: str) -> int:
-    """Missing Stars.
-
-    Function takes a string of space and new line separated numbers.
-    The string contains two lists of numbers.
-    Each line of the list contains items belonging to each list.
+def extract_lists(input_text: str):
+    """Extract the two lists of numbers from the input text.
 
     The string must be split,
     converted to integers,
     each pair of numbers assigned to list A or B,
     sorted.
 
-    Returns the difference between the two lists
+    Returns a pair of lists.
     """
+
     lines = input_text.split("\n")
     A = []
     B = []
-    answer = 0
 
     for line in lines:
         values = line.split("   ")
@@ -35,7 +31,35 @@ def missing_stars_solution(input_text: str) -> int:
     A.sort()
     B.sort()
 
+    return A, B
+
+
+def missing_stars_solution(input_text: str) -> int:
+    """Missing Stars.
+
+    Function takes a string of space and new line separated numbers.
+    The string contains two lists of numbers.
+    Each line of the list contains items belonging to each list.
+
+    Returns the difference between the two lists
+    """
+
+    answer = 0
+
+    A, B = extract_lists(input_text)
+
     for a, b in zip(A, B):
         answer += max(a - b, b -a)
 
     return answer
+
+
+def similarity_score(input_text: str) -> int:
+    """Missing Stars: part 2.
+
+    Calculate the number of times an integer appears in a second list.
+    Multiply it by the number of occurrences.
+
+    Return the sum of the first list.
+    """
+    return 0
