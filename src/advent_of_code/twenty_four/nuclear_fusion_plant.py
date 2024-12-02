b517e5538dc077e445c43ab177b0d008790a3a8e
James <james@makejames.com>
2024-12-02 16:58:37 +0000

    FUNC: day two part 1 solution now passing.

--- src/advent_of_code/twenty_four/nuclear_fusion_plant.py
+++ src/advent_of_code/twenty_four/nuclear_fusion_plant.py
@@ -15,5 +15,41 @@ def safe_reports(input_text: str) -> int:
 
     Returns the number of safe reports
     """
+    answer = 0
+
+    reports = input_text.split("\n")
+
+    for report in reports:
+
+        if report == "":
+            continue
+
+        levels = [int(val) for val in report.split(" ")]
+        safe = True
+
+        direction = ""
+
+        for level, next_level in zip(levels, levels[1:]):
+
+            if level >= next_level:
+                if direction == "ascending":
+                    safe = False
+                    break
+                direction = "descending"
+
+            if level <= next_level:
+                if direction == "descending":
+                    safe = False
+                    break
+                direction = "ascending"
+
+            if max((level - next_level), (next_level - level)) > 3:
+                safe = False
+                break
+
+        if safe is True:
+            answer += 1
+
+    return answer
 
     return 0
