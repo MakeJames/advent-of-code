acde501b6a82295b2f0e8509f0343b0e838ed5ad
makejames <james@makejames.com>
2024-12-02 19:47:49 +0000

    TEST: add performance test

--- src/advent_of_code/common.py
+++ src/advent_of_code/common.py
@@ -2,10 +2,14 @@
 
 
 from pathlib import Path
-from random import randint
+from random import randint, random
 
 from advent_of_code.twenty_four.missing_stars import missing_stars_solution
+from advent_of_code.twenty_four.nuclear_fusion_plant import safe_reports
 
+
+def random_int(start: int, stop: int) -> int:
+    return randint(start, stop)
 
 def random_with_N_digits(n: int) -> int:
     if n < 0:
@@ -31,6 +35,37 @@ def create_missing_stars_test_data(lines: int=25, digits: int=4) -> None:
     test_string = "\n".join(data)
     result = missing_stars_solution(test_string)
     file_name = f"missing_stars_{lines}_{digits}_{result}.txt"
+
+    file = Path(__file__).parent.parent.parent.joinpath("tests", "twenty_four",
+                                                        "test_data", file_name)
+    file.write_text(test_string)
+    print(f"New test data written to '{file}'")
+
+
+def create_nuclear_reports_test_data(reports: int) -> None:
+    test_reports = []
+
+    for i in range(reports):
+        report = []
+        val = random_int(-100000, 200000)
+
+        for i in range(5):
+            report.append(f"{val}")
+            new_vals = [val, (val + random_int(0, 7))]
+            if random() == 1:
+                new_vals = [val, (val - random_int(0, 7))]
+            if random() == 1:
+                new_vals = [val, (val * random_int(1, 3))]
+            if random() == 1:
+                new_vals = [val, int(val / random_int(1, 3))]
+            val = random_int(min(new_vals), max(new_vals))
+
+        print(report)
+        test_reports.append(" ".join(report))
+
+    test_string = "\n".join(test_reports)
+    result = safe_reports(test_string)
+    file_name = f"nuclear_reports_{reports}_{result}.txt"
 
     file = Path(__file__).parent.parent.parent.joinpath("tests", "twenty_four",
                                                         "test_data", file_name)
--- tests/twenty_four/test_nuclear_fusion_plant.py
+++ tests/twenty_four/test_nuclear_fusion_plant.py
@@ -2,6 +2,8 @@
 
 import pytest
 
+from pathlib import Path
+
 from advent_of_code.twenty_four.nuclear_fusion_plant import safe_reports
 
 
@@ -12,9 +14,33 @@ from advent_of_code.twenty_four.nuclear_fusion_plant import safe_reports
             "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9\n",
             2,
         ),
+        (
+            "9 5 6 3 6\n4 5 7 8 10\n9 7 6 5 4\n4 4 4 4 4\n-1 -2 -3 -4 -6\n1 5 6 7 4\n",
+            3,
+        ),
+        (
+            "9 5 6 3 6\n4 5 7 8 10\n9 7 6 5 4\n4 4 4 4 4\n-1 -2 -3 -4 -6\n1 5 6 7 4\n",
+            3,
+        ),
     ],
 )
 def test_when_valid_input_return_correct(test_input, expected) -> None:
     """R-BICEP: Right."""
     result = safe_reports(test_input)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("nuclear_reports_1000_56.txt", 56),
+        ("nuclear_reports_2000000_104626.txt", 104626),
+        ("nuclear_reports_20000_1049.txt", 1049),
+    ]
+)
+def test_nuclear_large_files_and_numbers(test_input: str, expected: int, benchmark) -> None:
+    """R-BICEP: Performance."""
+    test_file = Path(__file__).parent.joinpath("test_data", test_input)
+    result = benchmark(safe_reports, test_file.read_text())
+
     assert result == expected
